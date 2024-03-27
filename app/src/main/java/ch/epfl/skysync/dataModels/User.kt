96d616fccfcb6b376d415dba212b9e8cd2bc13fa
rafi <rafael.sollberger@gmail.com>
2024-03-27 13:48:29 +0100

    base class for users added

---
+++ app/src/main/java/ch/epfl/skysync/dataModels/User.kt
@@ -0,0 +1,10 @@
+package ch.epfl.skysync.dataModels
+
+interface  User {
+    val firstname: String
+    val lastname: String
+    val userId: String
+    //val availabilities: Calendar<Availability>
+    //val assignedFlights: Calendar<PlannedFlight>
+
+}
