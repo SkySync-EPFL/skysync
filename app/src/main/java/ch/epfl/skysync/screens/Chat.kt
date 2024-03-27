65526530d854ff88ed883a554aff41ffd121949e
Baribal02 <guillaume.mattheydoret@gmail.com>
2024-03-27 11:50:06 +0100

    Scaffold implementation
    Implemenation of the scaffold wiht the bottom navigation bar in the main group of screens : calendar, chat, flight and home

--- app/src/main/java/ch/epfl/skysync/screens/Chat.kt
+++ app/src/main/java/ch/epfl/skysync/screens/Chat.kt
@@ -3,30 +3,41 @@ package ch.epfl.skysync.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ch.epfl.skysync.navigation.BottomBar
 
 @Composable
-fun ChatScreen() {
-  Box(
-      modifier = Modifier.fillMaxSize().background(Color.Red),
-      contentAlignment = Alignment.Center) {
-        Text(
+fun ChatScreen(navController : NavHostController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = { BottomBar(navController) }
+    )
+    { padding ->
+        androidx.compose.material3.Text(
+            modifier = Modifier.padding(padding),
             text = "Chat",
-            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontSize = androidx.compose.material3.MaterialTheme.typography.displayLarge.fontSize,
             fontWeight = FontWeight.Bold,
-            color = Color.White)
-      }
+            color = Color.Black
+        )
+    }
 }
 
 @Composable
 @Preview
 fun ChatScreenPreview() {
-  ChatScreen()
+    val navController = rememberNavController()
+    ChatScreen(navController = navController)
 }
--- app/src/main/java/ch/epfl/skysync/screens/Flight.kt
+++ app/src/main/java/ch/epfl/skysync/screens/Flight.kt
@@ -3,30 +3,41 @@ package ch.epfl.skysync.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ch.epfl.skysync.navigation.BottomBar
 
 @Composable
-fun FlightScreen() {
-  Box(
-      modifier = Modifier.fillMaxSize().background(Color.Blue),
-      contentAlignment = Alignment.Center) {
-        Text(
+fun FlightScreen(navController : NavHostController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = { BottomBar(navController) }
+    )
+    { padding ->
+        androidx.compose.material3.Text(
+            modifier = Modifier.padding(padding),
             text = "Flight",
-            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontSize = androidx.compose.material3.MaterialTheme.typography.displayLarge.fontSize,
             fontWeight = FontWeight.Bold,
-            color = Color.White)
-      }
+            color = Color.Black
+        )
+    }
 }
 
 @Composable
 @Preview
 fun FlightScreenPreview() {
-  FlightScreen()
+    val navController = rememberNavController()
+    FlightScreen(navController = navController)
 }
