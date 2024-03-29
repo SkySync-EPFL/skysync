89cf8375db6e13b87e7522deb842ec803869e279
rafi <rafael.sollberger@gmail.com>
2024-03-29 10:24:57 +0100

    team doc and test added

--- app/src/main/java/ch/epfl/skysync/models/flight/Team.kt
+++ app/src/main/java/ch/epfl/skysync/models/flight/Team.kt
@@ -2,31 +2,36 @@ package ch.epfl.skysync.models.flight
 
 import ch.epfl.skysync.models.user.User
 
-
+/**
+ * represents group of roles that belong to one flight operation
+ * @property roles the list of roles that belong to one flight operation
+ * (immutable class)
+ */
 data class Team (val roles:List<Role>)
 
 
 {
     /**
      * checks if every role has a user assigned to it.
-     * @return True if every role has a user assigned to it.
+     * @return True if at least one role and every role has a user assigned to it.
      */
     fun isComplete(): Boolean {
-        return roles.all { it.isAssigned()}
+        return roles.isNotEmpty() && roles.all { it.isAssigned()}
     }
 
-
+    /**
+     * assigns the given user to the first role with the given role type
+     */
     fun assign(user: User, roleType: RoleType): Team {
-        if (!user.canAssumeRole(roleType)) throw IllegalArgumentException("user can not assume role type")
         //Todo: to be implemented
         throw NotImplementedError()
 
     }
 
-    fun hasNoRoles(): Boolean {
-        return roles.isEmpty()
-    }
-
+    /**
+     * @param rolesToAdd: the roles that will be added to this team
+     * @return new team instance with the added roles
+     */
     fun addRoles(rolesToAdd: List<Role>): Team{
         val newRoles =  rolesToAdd + roles
         return Team(newRoles)
