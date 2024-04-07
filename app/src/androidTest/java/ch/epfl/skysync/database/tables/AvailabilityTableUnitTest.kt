ecba3b0cb3b1e8911bc524f3c846cf00dbbb22c5
Alexandre Goumaz <alexandre.k.goumaz@gmail.com>
2024-04-07 11:40:32 +0200

    format files

--- app/src/androidTest/java/ch/epfl/skysync/database/tables/AvailabilityTableUnitTest.kt
+++ app/src/androidTest/java/ch/epfl/skysync/database/tables/AvailabilityTableUnitTest.kt
@@ -6,122 +6,112 @@ import ch.epfl.skysync.database.FirestoreDatabase
 import ch.epfl.skysync.models.calendar.Availability
 import ch.epfl.skysync.models.calendar.AvailabilityStatus
 import ch.epfl.skysync.models.calendar.TimeSlot
+import java.time.LocalDate
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.time.LocalDate
 
 // sleep to let the db requests the time to be done
 const val DB_SLEEP_TIME = 1000L
 
 @RunWith(AndroidJUnit4::class)
 class AvailabilityTableUnitTest {
-    private val table = AvailabilityTable(FirestoreDatabase(useEmulator = true))
+  private val table = AvailabilityTable(FirestoreDatabase(useEmulator = true))
 
-    @Before
-    fun testSetup() {
-        table.deleteTable({})
-        SystemClock.sleep(DB_SLEEP_TIME)
-    }
+  @Before
+  fun testSetup() {
+    table.deleteTable({})
+    SystemClock.sleep(DB_SLEEP_TIME)
+  }
 
-    /**
-     * Test the AvailabilityTable as one integration test
-     * as it is mostly a wrapper of the FirestoreDatabase class (it has no relations)
-     * and the FirestoreDatabase has no mock class, instead we use an emulator,
-     * meaning we can not test the `add` method in isolation for example.
-     */
-    @Test
-    fun integrationTest() {
-        val personId = "personId"
-        val availability =
-            Availability(
-                status = AvailabilityStatus.MAYBE,
-                timeSlot = TimeSlot.PM,
-                date = LocalDate.now()
-            )
+  /**
+   * Test the AvailabilityTable as one integration test as it is mostly a wrapper of the
+   * FirestoreDatabase class (it has no relations) and the FirestoreDatabase has no mock class,
+   * instead we use an emulator, meaning we can not test the `add` method in isolation for example.
+   */
+  @Test
+  fun integrationTest() {
+    val personId = "personId"
+    val availability =
+        Availability(
+            status = AvailabilityStatus.MAYBE, timeSlot = TimeSlot.PM, date = LocalDate.now())
 
-        // Step 1: Add
+    // Step 1: Add
 
-        var id = "__invalid_id__"
-        var addComplete = false
-        var addError = false
+    var id = "__invalid_id__"
+    var addComplete = false
+    var addError = false
 
-        // add an availability
-        table.add(
-            personId,
-            availability,
-            {
-                id = it
-                addComplete = true
-            },
-            { addError = true })
+    // add an availability
+    table.add(
+        personId,
+        availability,
+        {
+          id = it
+          addComplete = true
+        },
+        { addError = true })
 
-        SystemClock.sleep(DB_SLEEP_TIME)
+    SystemClock.sleep(DB_SLEEP_TIME)
 
-        assertEquals(true, addComplete)
-        assertEquals(false, addError)
+    assertEquals(true, addComplete)
+    assertEquals(false, addError)
 
-        // Step 2: Get
+    // Step 2: Get
 
-        var getAvailability: Availability? = null
-        var getComplete = false
-        var getError = false
+    var getAvailability: Availability? = null
+    var getComplete = false
+    var getError = false
 
-        // retrieve the added availability
-        table.get(
-            id,
-            {
-                getAvailability = it
-                getComplete = true
-            },
-            { getError = true })
+    // retrieve the added availability
+    table.get(
+        id,
+        {
+          getAvailability = it
+          getComplete = true
+        },
+        { getError = true })
 
-        SystemClock.sleep(DB_SLEEP_TIME)
+    SystemClock.sleep(DB_SLEEP_TIME)
 
-        assertEquals(true, getComplete)
-        assertEquals(false, getError)
-        // the added then retrieved availability should be the same as the initial one
-        assertEquals(availability.copy(id = id), getAvailability)
+    assertEquals(true, getComplete)
+    assertEquals(false, getError)
+    // the added then retrieved availability should be the same as the initial one
+    assertEquals(availability.copy(id = id), getAvailability)
 
-        // Step 3: Delete
+    // Step 3: Delete
 
-        var deleteComplete = false
-        var deleteError = false
+    var deleteComplete = false
+    var deleteError = false
 
-        // delete the availability
-        table.delete(
-            id,
-            {
-                deleteComplete = true
-            },
-            { deleteError = true })
+    // delete the availability
+    table.delete(id, { deleteComplete = true }, { deleteError = true })
 
-        SystemClock.sleep(DB_SLEEP_TIME)
+    SystemClock.sleep(DB_SLEEP_TIME)
 
-        assertEquals(true, deleteComplete)
-        assertEquals(false, deleteError)
+    assertEquals(true, deleteComplete)
+    assertEquals(false, deleteError)
 
-        // Step 4: GetAll
+    // Step 4: GetAll
 
-        var getAllSize = -1
-        var getAllComplete = false
-        var getAllError = false
+    var getAllSize = -1
+    var getAllComplete = false
+    var getAllError = false
 
-        // get all the availabilities
-        table.getAll(
-            {
-                getAllSize = it.size
-                getAllComplete = true
-            },
-            { getAllError = true })
+    // get all the availabilities
+    table.getAll(
+        {
+          getAllSize = it.size
+          getAllComplete = true
+        },
+        { getAllError = true })
 
-        SystemClock.sleep(DB_SLEEP_TIME)
+    SystemClock.sleep(DB_SLEEP_TIME)
 
-        assertEquals(true, getAllComplete)
-        assertEquals(false, getAllError)
-        // there should not be any availabilities left
-        assertEquals(0, getAllSize)
-    }
+    assertEquals(true, getAllComplete)
+    assertEquals(false, getAllError)
+    // there should not be any availabilities left
+    assertEquals(0, getAllSize)
+  }
 }
-
--- app/src/main/java/ch/epfl/skysync/database/tables/AvailabilityTable.kt
+++ app/src/main/java/ch/epfl/skysync/database/tables/AvailabilityTable.kt
@@ -7,80 +7,73 @@ import ch.epfl.skysync.models.calendar.Availability
 /** Represent the "availability" table */
 class AvailabilityTable(private val db: FirestoreDatabase) {
 
-    /**
-     * Get an availability by ID
-     *
-     * @param id The id of the availability
-     * @param onCompletion Callback called on completion of the operation
-     * @param onError Callback called when an error occurs
-     */
-    fun get(id: String, onCompletion: (Availability?) -> Unit, onError: (Exception) -> Unit) {
-        db.getItem(PATH, id, AvailabilitySchema::class, { onCompletion(it?.toModel()) }, onError)
-    }
+  /**
+   * Get an availability by ID
+   *
+   * @param id The id of the availability
+   * @param onCompletion Callback called on completion of the operation
+   * @param onError Callback called when an error occurs
+   */
+  fun get(id: String, onCompletion: (Availability?) -> Unit, onError: (Exception) -> Unit) {
+    db.getItem(PATH, id, AvailabilitySchema::class, { onCompletion(it?.toModel()) }, onError)
+  }
 
-    /**
-     * Get all the availabilities
-     *
-     * @param onCompletion Callback called on completion of the operation
-     * @param onError Callback called when an error occurs
-     */
-    fun getAll(onCompletion: (List<Availability>) -> Unit, onError: (Exception) -> Unit) {
-        db.getAll(
-            PATH,
-            AvailabilitySchema::class,
-            { schemas -> onCompletion(schemas.map { it.toModel() }) },
-            onError
-        )
-    }
+  /**
+   * Get all the availabilities
+   *
+   * @param onCompletion Callback called on completion of the operation
+   * @param onError Callback called when an error occurs
+   */
+  fun getAll(onCompletion: (List<Availability>) -> Unit, onError: (Exception) -> Unit) {
+    db.getAll(
+        PATH,
+        AvailabilitySchema::class,
+        { schemas -> onCompletion(schemas.map { it.toModel() }) },
+        onError)
+  }
 
-    /**
-     * Add a new availability to the database
-     *
-     * This will generate a new id for this availability and override any previously set id.
-     *
-     * @param personId The ID of the person whose availability it is
-     * @param item The availability to add to the database
-     * @param onCompletion Callback called on completion of the operation
-     * @param onError Callback called when an error occurs
-     */
-    fun add(
-        personId: String,
-        item: Availability,
-        onCompletion: (id: String) -> Unit,
-        onError: (Exception) -> Unit
-    ) {
-        db.addItem(PATH, AvailabilitySchema.fromModel(personId, item), onCompletion, onError)
-    }
+  /**
+   * Add a new availability to the database
+   *
+   * This will generate a new id for this availability and override any previously set id.
+   *
+   * @param personId The ID of the person whose availability it is
+   * @param item The availability to add to the database
+   * @param onCompletion Callback called on completion of the operation
+   * @param onError Callback called when an error occurs
+   */
+  fun add(
+      personId: String,
+      item: Availability,
+      onCompletion: (id: String) -> Unit,
+      onError: (Exception) -> Unit
+  ) {
+    db.addItem(PATH, AvailabilitySchema.fromModel(personId, item), onCompletion, onError)
+  }
 
-    /**
-     * Delete an availability from the database
-     *
-     * @param id The id of the availability
-     * @param onCompletion Callback called on completion of the operation
-     * @param onError Callback called when an error occurs
-     */
-    fun delete(
-        id: String,
-        onCompletion: () -> Unit,
-        onError: (java.lang.Exception) -> Unit
-    ) {
-        db.deleteItem(PATH, id, onCompletion, onError)
-    }
+  /**
+   * Delete an availability from the database
+   *
+   * @param id The id of the availability
+   * @param onCompletion Callback called on completion of the operation
+   * @param onError Callback called when an error occurs
+   */
+  fun delete(id: String, onCompletion: () -> Unit, onError: (java.lang.Exception) -> Unit) {
+    db.deleteItem(PATH, id, onCompletion, onError)
+  }
 
-    /**
-     * Delete the table
-     *
-     * This is only used for testing, as such it is only supported if using the emulator.
-     *
-     * @param onError Callback called when an error occurs
-     */
-    fun deleteTable(
-        onError: (Exception) -> Unit
-    ) {
-        db.deleteTable(PATH, onError)
-    }
+  /**
+   * Delete the table
+   *
+   * This is only used for testing, as such it is only supported if using the emulator.
+   *
+   * @param onError Callback called when an error occurs
+   */
+  fun deleteTable(onError: (Exception) -> Unit) {
+    db.deleteTable(PATH, onError)
+  }
 
-    companion object {
-        const val PATH = "availability"
-    }
+  companion object {
+    const val PATH = "availability"
+  }
 }
