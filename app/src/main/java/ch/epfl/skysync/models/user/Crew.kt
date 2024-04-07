9d22c39b11c87c144b22c0835c5b6c826a5684dd
Alexandre Goumaz <alexandre.k.goumaz@gmail.com>
2024-04-07 15:11:21 +0200

    user models add unset id

--- app/src/main/java/ch/epfl/skysync/models/user/Crew.kt
+++ app/src/main/java/ch/epfl/skysync/models/user/Crew.kt
@@ -1,13 +1,14 @@
 package ch.epfl.skysync.models.user
 
+import ch.epfl.skysync.models.UNSET_ID
 import ch.epfl.skysync.models.calendar.AvailabilityCalendar
 import ch.epfl.skysync.models.calendar.FlightGroupCalendar
 import ch.epfl.skysync.models.flight.RoleType
 
 class Crew(
+    override val id: String = UNSET_ID,
     override val firstname: String,
     override val lastname: String,
-    override val id: String,
     override val availabilities: AvailabilityCalendar,
     override val assignedFlights: FlightGroupCalendar,
 ) : User {
--- app/src/main/java/ch/epfl/skysync/models/user/Pilot.kt
+++ app/src/main/java/ch/epfl/skysync/models/user/Pilot.kt
@@ -1,14 +1,15 @@
 package ch.epfl.skysync.models.user
 
+import ch.epfl.skysync.models.UNSET_ID
 import ch.epfl.skysync.models.calendar.AvailabilityCalendar
 import ch.epfl.skysync.models.calendar.FlightGroupCalendar
 import ch.epfl.skysync.models.flight.BalloonQualification
 import ch.epfl.skysync.models.flight.RoleType
 
 class Pilot(
+    override val id: String = UNSET_ID,
     override val firstname: String,
     override val lastname: String,
-    override val id: String,
     override val availabilities: AvailabilityCalendar,
     override val assignedFlights: FlightGroupCalendar,
     val qualification: BalloonQualification,
