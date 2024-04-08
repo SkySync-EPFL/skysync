0c221d0c5998e16a25fc18a7084d79b7207e5fc3
Alexandre Goumaz <alexandre.k.goumaz@gmail.com>
2024-04-08 17:48:58 +0200

    fixup! add simple flight schemas/tables

--- app/src/main/java/ch/epfl/skysync/database/schemas/FlightMemberSchema.kt
+++ app/src/main/java/ch/epfl/skysync/database/schemas/FlightMemberSchema.kt
@@ -1,12 +1,14 @@
 package ch.epfl.skysync.database.schemas
-x
+
 import ch.epfl.skysync.database.Schema
+import ch.epfl.skysync.models.flight.RoleType
 import com.google.firebase.firestore.DocumentId
 
 data class FlightMemberSchema(
     @DocumentId val id: String? = null,
-    val name: String? = null,
-    val hasDoor: Boolean? = null,
+    val userId: String? = null,
+    val flightId: String? = null,
+    val roleType: RoleType? = null,
 ) : Schema<FlightMemberSchema> {
   override fun toModel(): FlightMemberSchema {
     return this
