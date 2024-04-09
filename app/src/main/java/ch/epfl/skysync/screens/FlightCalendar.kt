44e28e1b71a714b457068e8c283790a40e080892
lucca <lucca.guerinsaturnino@epfl.ch>
2024-04-09 15:22:33 +0200

    Switch Button

--- app/src/main/java/ch/epfl/skysync/screens/FlightCalendar.kt
+++ app/src/main/java/ch/epfl/skysync/screens/FlightCalendar.kt
@@ -1,10 +1,38 @@
 package ch.epfl.skysync.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import ch.epfl.skysync.models.calendar.TimeSlot
@@ -15,6 +43,7 @@ import ch.epfl.skysync.models.flight.Role
 import ch.epfl.skysync.models.flight.RoleType
 import ch.epfl.skysync.models.flight.Team
 import ch.epfl.skysync.models.flight.Vehicle
+import ch.epfl.skysync.navigation.Route
 import java.time.LocalDate
 
 /**
@@ -60,10 +89,60 @@ fun ShowFlightCalendar(
     getFlightByDate: (LocalDate, TimeSlot) -> Flight?,
     navController: NavHostController
 ) {
-  Box() {
-    Calendar { date, time, size -> ShowFlight(date, time, getFlightByDate, onClick) }
-    Button(onClick = { onClick() }) { Text("Add Flight") }
-  }
+    Box() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 64.dp), // Adjust bottom padding as needed
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray, RoundedCornerShape(16.dp))
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.White, RoundedCornerShape(16.dp))
+                        .size(width = 170.dp, height = 40.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Flight Calendar",
+                        fontSize = 12.sp,
+                        color = Color(0xFFFFA500),
+                        modifier = Modifier
+                            .padding(8.dp),
+                        overflow = TextOverflow.Clip
+                    )
+                }
+                Button(
+                    onClick = { navController.navigate(Route.CALENDAR) },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray),
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .size(width = 170.dp, height = 40.dp),
+                ) {
+                    Text(
+                        text = "Avaliability Calendar",
+                        fontSize = 12.sp,
+                        color = Color.Black,
+                        overflow = TextOverflow.Clip,
+                        maxLines = 1
+                    )
+                }
+            }
+        }
+        Calendar { date, time, size -> ShowFlight(date, time, getFlightByDate, onClick) }
+        Button(
+            onClick = {},
+            shape = CircleShape,
+            colors = ButtonDefaults.outlinedButtonColors(containerColor=  Color.Gray),
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "content description", tint = Color.DarkGray)
+        }
+    }
 }
 
 @Composable
