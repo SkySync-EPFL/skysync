7b3ea5a4e1f72902f89e5034970fadb578ca05e6
rafi <rafael.sollberger@gmail.com>
2024-04-10 17:11:44 +0200

    blueprint for two calendar screens

--- app/src/main/java/ch/epfl/skysync/screens/Calendar.kt
+++ app/src/main/java/ch/epfl/skysync/screens/Calendar.kt
@@ -1,34 +1,55 @@
 package ch.epfl.skysync.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import ch.epfl.skysync.navigation.BottomBar
+import ch.epfl.skysync.navigation.Route
+import ch.epfl.skysync.viewmodel.UserViewModel
 
 @Composable
-fun CalendarScreen(navController: NavHostController) {
+fun CalendarScreen(navController: NavHostController,
+                   calendarType: String,
+                   viewModel: UserViewModel) {
   Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { BottomBar(navController) }) { padding ->
-    Text(
-        modifier = Modifier.padding(padding),
-        text = "Calendar",
-        fontSize = MaterialTheme.typography.displayLarge.fontSize,
-        fontWeight = FontWeight.Bold,
-        color = Color.Black)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+
+    ){
+        if (calendarType == Route.AVAILABILITY_CALENDAR) {
+                showCalendarAvailabilities(navController,padding, viewModel)
+            }
+        else if (calendarType == Route.PERSONAL_FLIGHT_CALENDAR) {
+            //TODO: change to correct view model
+                showCalendarAvailabilities(navController, padding, viewModel)
+            }
+      Row {
+          Button(onClick = { /*TODO*/ }) {
+              Text("Flights")
+
+          }
+          Button(onClick = { /*TODO*/ }) {
+              Text("Availabilities")
+
+          }
+
+      }
+    }
   }
 }
 
-@Composable
-@Preview
-fun CalendarScreenPreview() {
-  val navController = rememberNavController()
-  CalendarScreen(navController = navController)
-}
+
+
+
