e0690176123e69eedcc5d65cc02745389ac962e5
Prolas <timo.moebel@hotmail.com>
2024-04-11 20:23:41 +0200

    Add Tests LoginScreen

--- app/src/androidTest/java/ch/epfl/skysync/screens/flight/FlightScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/skysync/screens/flight/FlightScreenTest.kt
@@ -2,11 +2,12 @@ package ch.epfl.skysync.screens.flight
 
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
-import ch.epfl.skysync.screens.ChatScreen
+import ch.epfl.skysync.screens.FlightScreen
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -21,12 +22,12 @@ class FlightScreenTest {
     composeTestRule.setContent {
       navController = TestNavHostController(LocalContext.current)
       navController.navigatorProvider.addNavigator(ComposeNavigator())
-      ChatScreen(navController)
+      FlightScreen(navController)
     }
   }
 
   @Test
   fun flightScreenIsDisplayed() {
-    composeTestRule.onNode(hasTestTag("FLightScreenScaffold\"")).assertHasClickAction()
+    composeTestRule.onNode(hasTestTag("FLightScreenScaffold")).assertIsDisplayed()
   }
 }
