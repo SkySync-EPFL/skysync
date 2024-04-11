648dbe65d26c2634a439db4fc56d50741b599f5c
Prolas <timo.moebel@hotmail.com>
2024-04-11 17:56:51 +0200

    Add tests for flight and chat

---
+++ app/src/androidTest/java/ch/epfl/skysync/screens/chat/ChatScreenTest.kt
@@ -0,0 +1,37 @@
+package ch.epfl.skysync.screens.chat
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.testing.TestNavHostController
+import ch.epfl.skysync.navigation.Route
+import ch.epfl.skysync.navigation.homeGraph
+import ch.epfl.skysync.screens.ChatScreen
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ChatScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setUpNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            ChatScreen(navController)
+        }
+    }
+
+    @Test
+    fun chatScreenIsDisplayed() {
+        composeTestRule.onNode(hasTestTag("ChatScreenScaffold")).assertHasClickAction()
+    }
+}
