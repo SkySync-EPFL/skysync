925cc205b62dd3da664121c90746bc48393b9a4c
Prolas <timo.moebel@hotmail.com>
2024-04-11 17:54:23 +0200

    Chat and Flight tests

--- app/src/main/java/ch/epfl/skysync/screens/Chat.kt
+++ app/src/main/java/ch/epfl/skysync/screens/Chat.kt
@@ -7,16 +7,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import ch.epfl.skysync.navigation.BottomBar
-import javax.annotation.processing.Generated
+import kotlin.annotation.AnnotationRetention
+import kotlin.annotation.Retention
+
+@Retention(AnnotationRetention.RUNTIME) // Set retention policy to runtime
+annotation class ExcludeGenerated
 
 @Composable
 fun ChatScreen(navController: NavHostController) {
-  Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { BottomBar(navController) }) { padding ->
+  Scaffold(modifier = Modifier.fillMaxSize().testTag("ChatScreenScaffold"), bottomBar = { BottomBar(navController) }) { padding ->
     Text(
         modifier = Modifier.padding(padding),
         text = "Chat",
@@ -25,11 +30,15 @@ fun ChatScreen(navController: NavHostController) {
         color = Color.Black)
   }
 }
+@ExcludeGenerated
+fun test(): String {
+  return "Yes"
+}
 
-@Generated
 @Composable
 @Preview
-fun ChatScreenPreview() {
+@ExcludePreviewFromTestReport
+fun Preview() {
   val navController = rememberNavController()
   ChatScreen(navController = navController)
 }
