72ff2617276c991a25f88c7b0a9142f550118c71
Baribal02 <guillaume.mattheydoret@gmail.com>
2024-04-11 23:21:13 +0200

    Formatted version

--- app/src/main/java/ch/epfl/skysync/models/flight/PlannedFlight.kt
+++ app/src/main/java/ch/epfl/skysync/models/flight/PlannedFlight.kt
@@ -3,7 +3,6 @@ package ch.epfl.skysync.models.flight
 import ch.epfl.skysync.models.calendar.TimeSlot
 import java.time.LocalDate
 
-
 class PlannedFlight(
     override val id: String?,
     override val nPassengers: Int,
