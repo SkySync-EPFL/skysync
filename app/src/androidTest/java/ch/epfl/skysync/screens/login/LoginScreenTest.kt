2f207068f50e4101108fc2f2a65b32257a02abdc
Prolas <timo.moebel@hotmail.com>
2024-04-12 09:49:57 +0200

    Add package name

--- app/src/androidTest/java/ch/epfl/skysync/screens/login/LoginScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/skysync/screens/login/LoginScreenTest.kt
@@ -1,3 +1,6 @@
+package ch.epfl.skysync.screens.login
+
+/*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
@@ -11,7 +14,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/*
+/
 @RunWith(AndroidJUnit4::class)
 class LoginScreenTest : TestCase() {
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
