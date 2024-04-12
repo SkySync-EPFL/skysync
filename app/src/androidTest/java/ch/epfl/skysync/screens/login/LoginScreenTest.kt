019c0b781b1f559da9477eb663de76078eea702c
Prolas <timo.moebel@hotmail.com>
2024-04-12 10:23:24 +0200

    LoginScreenTest once again

--- app/src/androidTest/java/ch/epfl/skysync/screens/login/LoginScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/skysync/screens/login/LoginScreenTest.kt
@@ -1,6 +1,6 @@
 package ch.epfl.skysync.screens.login
 
-/*
+
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
@@ -14,7 +14,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/
+
 @RunWith(AndroidJUnit4::class)
 class LoginScreenTest : TestCase() {
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
@@ -45,4 +45,4 @@ class LoginScreenTest : TestCase() {
     }
   }
 }
-*/
+
