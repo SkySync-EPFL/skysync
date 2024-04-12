c2223694c8bfe5b2289b73b54e3b4d37b03d5663
Baribal02 <guillaume.mattheydoret@gmail.com>
2024-04-12 08:35:11 +0200

    Formated version

--- app/src/main/java/ch/epfl/skysync/navigation/MainGraph.kt
+++ app/src/main/java/ch/epfl/skysync/navigation/MainGraph.kt
@@ -16,10 +16,9 @@ fun MainGraph(
     signInLauncher: ActivityResultLauncher<Intent>,
     user: FirebaseUser?
 ) {
-  NavHost(
-      navController = navHostController, startDestination = Route.MAIN){
-      //startDestination = if (user == null) Route.LOGIN else Route.MAIN) {
-        homeGraph(navHostController, user)
-        composable(Route.LOGIN) { LoginScreen(signInLauncher = signInLauncher) }
-      }
+  NavHost(navController = navHostController, startDestination = Route.MAIN) {
+    // startDestination = if (user == null) Route.LOGIN else Route.MAIN) {
+    homeGraph(navHostController, user)
+    composable(Route.LOGIN) { LoginScreen(signInLauncher = signInLauncher) }
+  }
 }
