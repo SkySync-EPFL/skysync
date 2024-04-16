1bc8d8aeaa7b743c86326f9afbd47ccdf5bfa7ca
rafi <rafael.sollberger@gmail.com>
2024-04-16 08:24:03 +0200

    added method to transform a PlannedFlight into a ConfirmedFlight

--- app/src/main/java/ch/epfl/skysync/models/flight/PlannedFlight.kt
+++ app/src/main/java/ch/epfl/skysync/models/flight/PlannedFlight.kt
@@ -2,6 +2,7 @@ package ch.epfl.skysync.models.flight
 
 import ch.epfl.skysync.models.calendar.TimeSlot
 import java.time.LocalDate
+import java.time.LocalTime
 
 data class PlannedFlight(
     override val id: String,
@@ -15,6 +16,44 @@ data class PlannedFlight(
     override val vehicles: List<Vehicle>
 ) : Flight {
   fun readyToBeConfirmed(): Boolean {
-    return team.isComplete() && nPassengers > 0 && balloon != null && basket != null
+    return team.isComplete() &&
+            nPassengers > 0 &&
+            balloon != null &&
+            basket != null &&
+            vehicles.isNotEmpty()
   }
+
+    fun confirmFlight(
+        notReadyToConfirm: ()->Unit,
+        meetupTimeTeam: LocalTime,
+        departureTimeTeam: LocalTime,
+        meetupTimePassenger: LocalTime,
+        meetupLocationPassenger: String,
+        remarks: List<String>,
+        color: FlightColor
+
+
+    ): ConfirmedFlight {
+        if (!readyToBeConfirmed()) {
+            notReadyToConfirm()
+        }
+        return ConfirmedFlight(
+            id,
+            nPassengers,
+            team,
+            flightType,
+            balloon!!,
+            basket!!,
+            date,
+            timeSlot,
+            vehicles,
+            remarks = remarks,
+            color = color,
+            meetupTimeTeam = meetupTimeTeam,
+            departureTimeTeam = departureTimeTeam,
+            meetupTimePassenger = meetupTimePassenger,
+            meetupLocationPassenger = meetupLocationPassenger
+
+        )
+    }
 }
