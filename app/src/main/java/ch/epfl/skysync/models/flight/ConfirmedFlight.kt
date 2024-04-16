c1e87b36e87401a1cbaa5149835fa29bb197bf51
rafi <rafael.sollberger@gmail.com>
2024-04-16 08:23:39 +0200

    added ConfirmedFlight and FlightColor

---
+++ app/src/main/java/ch/epfl/skysync/models/flight/ConfirmedFlight.kt
@@ -0,0 +1,31 @@
+package ch.epfl.skysync.models.flight
+
+import androidx.compose.ui.graphics.Color
+import ch.epfl.skysync.models.calendar.TimeSlot
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class ConfirmedFlight(
+    override val id: String,
+    override val nPassengers: Int,
+    override val team: Team,
+    override val flightType: FlightType,
+    override val balloon: Balloon,
+    override val basket: Basket,
+    override val date: LocalDate,
+    override val timeSlot: TimeSlot,
+    override val vehicles: List<Vehicle>,
+    val remarks: List<String>,
+    val color: FlightColor = FlightColor.NO_COLOR,
+    val meetupTimeTeam: LocalTime,
+    val departureTimeTeam: LocalTime,
+    val meetupTimePassenger: LocalTime,
+    val meetupLocationPassenger: String,
+
+
+) : Flight {
+
+
+
+
+}
---
+++ app/src/main/java/ch/epfl/skysync/models/flight/FlightColor.kt
@@ -0,0 +1,10 @@
+package ch.epfl.skysync.models.flight
+
+enum class FlightColor {
+    RED,
+    BLUE,
+    ORANGE,
+    YELLOW,
+    PINK,
+    NO_COLOR
+}
