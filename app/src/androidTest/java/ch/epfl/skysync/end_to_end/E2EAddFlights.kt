a2bbc7de6240929f49a16df06d751dc84ba54985
mehenni <ryan.mehenni@epfl.ch>
2024-04-24 15:41:08 +0200

    Added simple endtoend test

--- app/src/androidTest/java/ch/epfl/skysync/end_to_end/E2EAddFlights.kt
+++ app/src/androidTest/java/ch/epfl/skysync/end_to_end/E2EAddFlights.kt
@@ -1,6 +1,5 @@
 package ch.epfl.skysync.end_to_end
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -12,85 +11,83 @@ import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.testing.TestNavHostController
 import ch.epfl.skysync.Repository
 import ch.epfl.skysync.database.FirestoreDatabase
 import ch.epfl.skysync.navigation.Route
 import ch.epfl.skysync.navigation.homeGraph
-import ch.epfl.skysync.screens.LoginScreen
-import com.google.firebase.auth.FirebaseUser
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class E2EAddFlights {
-    @get:Rule val composeTestRule = createComposeRule()
-    lateinit var navController: TestNavHostController
+  @get:Rule val composeTestRule = createComposeRule()
+  lateinit var navController: TestNavHostController
 
-    @Before
-    fun setUpNavHost() {
-        composeTestRule.setContent {
-            val db: FirestoreDatabase = FirestoreDatabase()
-            val repository: Repository = Repository(db)
-            navController = TestNavHostController(LocalContext.current)
-            navController.navigatorProvider.addNavigator(ComposeNavigator())
-            NavHost(navController = navController, startDestination = Route.MAIN) {
-                    homeGraph(repository, navController,null)
-                }
-            }
-        }
-
-    @Test
-    fun addFlightAsAdmin(){
-
-        composeTestRule.onNodeWithTag("addFlightButton").performClick()
-        var route = navController.currentBackStackEntry?.destination?.route
-        Assert.assertEquals(Route.ADD_FLIGHT, route)
-        val title: String = "Modify Flight"
-        composeTestRule.waitForIdle()
-        composeTestRule.onNodeWithTag("Flight Lazy Column").performScrollToNode(hasTestTag("nb Passenger"))
-        composeTestRule.onNodeWithTag("nb Passenger").performClick()
-        composeTestRule.onNodeWithTag("nb Passenger").performTextClearance()
-        composeTestRule.onNodeWithTag("nb Passenger").performTextInput("1")
-
-        composeTestRule
-            .onNodeWithTag("Flight Lazy Column")
-            .performScrollToNode(hasTestTag("Date Field"))
-        composeTestRule.onNodeWithTag("Date Field").performClick()
-        composeTestRule.onNodeWithText("OK").performClick()
-
-        composeTestRule
-            .onNodeWithTag("Flight Lazy Column")
-            .performScrollToNode(hasTestTag("Flight Type Menu"))
-        composeTestRule.onNodeWithTag("Flight Type Menu").performClick()
-        composeTestRule.waitForIdle()
-        composeTestRule.onNodeWithTag("Flight Type 1").performClick()
-
-        composeTestRule
-            .onNodeWithTag("Flight Lazy Column")
-            .performScrollToNode(hasTestTag("Vehicle 0 Menu"))
-        composeTestRule.onNodeWithTag("Vehicle 0 Menu").performClick()
-        composeTestRule.onNodeWithTag("Vehicle 0 1").performClick()
-
-        composeTestRule
-            .onNodeWithTag("Flight Lazy Column")
-            .performScrollToNode(hasTestTag("Time Slot Menu"))
-        composeTestRule.onNodeWithTag("Time Slot Menu").performClick()
-        composeTestRule.onNodeWithTag("Time Slot 1").performClick()
-
-        composeTestRule
-            .onNodeWithTag("Flight Lazy Column")
-            .performScrollToNode(hasTestTag("Balloon Menu"))
-        composeTestRule.onNodeWithTag("Balloon Menu").performClick()
-        composeTestRule.onNodeWithTag("Balloon 1").performClick()
-
-        composeTestRule
-            .onNodeWithTag("Flight Lazy Column")
-            .performScrollToNode(hasTestTag("Basket Menu"))
-        composeTestRule.onNodeWithTag("Basket Menu").performClick()
-        composeTestRule.onNodeWithTag("Basket 1").performClick()
+  @Before
+  fun setUpNavHost() {
+    composeTestRule.setContent {
+      val db: FirestoreDatabase = FirestoreDatabase()
+      val repository: Repository = Repository(db)
+      navController = TestNavHostController(LocalContext.current)
+      navController.navigatorProvider.addNavigator(ComposeNavigator())
+      NavHost(navController = navController, startDestination = Route.MAIN) {
+        homeGraph(repository, navController, null)
+      }
     }
+  }
 
+  @Test
+  fun addFlightAsAdmin() {
+
+    composeTestRule.onNodeWithTag("addFlightButton").performClick()
+    var route = navController.currentBackStackEntry?.destination?.route
+    Assert.assertEquals(Route.ADD_FLIGHT, route)
+    val title: String = "Modify Flight"
+    composeTestRule.waitForIdle()
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("nb Passenger"))
+    composeTestRule.onNodeWithTag("nb Passenger").performClick()
+    composeTestRule.onNodeWithTag("nb Passenger").performTextClearance()
+    composeTestRule.onNodeWithTag("nb Passenger").performTextInput("1")
+
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("Date Field"))
+    composeTestRule.onNodeWithTag("Date Field").performClick()
+    composeTestRule.onNodeWithText("OK").performClick()
+
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("Flight Type Menu"))
+    composeTestRule.onNodeWithTag("Flight Type Menu").performClick()
+    composeTestRule.waitForIdle()
+    composeTestRule.onNodeWithTag("Flight Type 1").performClick()
+
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("Vehicle 0 Menu"))
+    composeTestRule.onNodeWithTag("Vehicle 0 Menu").performClick()
+    composeTestRule.onNodeWithTag("Vehicle 0 1").performClick()
+
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("Time Slot Menu"))
+    composeTestRule.onNodeWithTag("Time Slot Menu").performClick()
+    composeTestRule.onNodeWithTag("Time Slot 1").performClick()
+
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("Balloon Menu"))
+    composeTestRule.onNodeWithTag("Balloon Menu").performClick()
+    composeTestRule.onNodeWithTag("Balloon 1").performClick()
+
+    composeTestRule
+        .onNodeWithTag("Flight Lazy Column")
+        .performScrollToNode(hasTestTag("Basket Menu"))
+    composeTestRule.onNodeWithTag("Basket Menu").performClick()
+    composeTestRule.onNodeWithTag("Basket 1").performClick()
+  }
 }
