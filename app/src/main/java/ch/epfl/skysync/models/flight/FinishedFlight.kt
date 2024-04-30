ddc57f31f1129adfc1a5913416b70fc718fc6104
Baribal02 <guillaume.mattheydoret@gmail.com>
2024-04-30 13:22:02 +0200

    Add of crew report values in the FinishedFlight

--- app/src/main/java/ch/epfl/skysync/models/flight/FinishedFlight.kt
+++ app/src/main/java/ch/epfl/skysync/models/flight/FinishedFlight.kt
@@ -1,6 +1,8 @@
 package ch.epfl.skysync.models.flight
 
+import android.location.Location
 import ch.epfl.skysync.models.calendar.TimeSlot
+import ch.epfl.skysync.models.user.Crew
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -15,8 +17,18 @@ data class FinishedFlight(
     override val timeSlot: TimeSlot,
     override val vehicles: List<Vehicle>,
     val color: FlightColor = FlightColor.NO_COLOR,
+    val takeOffTime: LocalTime,
+    val takeOffLocation: Location,
+    val landingTime: LocalTime,
+    val landingLocation: Location,
     val meetupTimePassenger: LocalTime,
-    val meetupLocationPassenger: String,
+    val breakTime: Long, // time in milliseconds
+    val vehicleProblems: List<String>,
+    val littleBottle: UInt,
+    val bigBottle: UInt,
+    val prestigeBottle: UInt,
+    val bottleToFarmer: Boolean,
+    val filledByCrew: Crew
 ) : Flight {
   override fun getFlightStatus(): FlightStatus {
     return FlightStatus.COMPLETED
