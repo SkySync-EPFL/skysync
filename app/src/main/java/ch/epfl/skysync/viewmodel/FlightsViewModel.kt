cf58b7f25564e2d1a98504c16be79b3f69d3b789
rafi <rafael.sollberger@gmail.com>
2024-04-30 16:34:28 +0200

    added synchronized user to FlightsModel

--- app/src/main/java/ch/epfl/skysync/viewmodel/FlightsViewModel.kt
+++ app/src/main/java/ch/epfl/skysync/viewmodel/FlightsViewModel.kt
@@ -14,6 +14,10 @@ import ch.epfl.skysync.models.flight.Flight
 import ch.epfl.skysync.models.flight.FlightType
 import ch.epfl.skysync.models.flight.PlannedFlight
 import ch.epfl.skysync.models.flight.Vehicle
+import ch.epfl.skysync.models.user.Admin
+import ch.epfl.skysync.models.user.Crew
+import ch.epfl.skysync.models.user.Pilot
+import ch.epfl.skysync.models.user.User
 import ch.epfl.skysync.util.WhileUiSubscribed
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -26,20 +30,18 @@ import kotlinx.coroutines.launch
 class FlightsViewModel(
     val repository: Repository,
     val userId: String?,
-    val userRole: UserRole
 ) : ViewModel() {
   companion object {
     @Composable
     fun createViewModel(
         repository: Repository,
         userId: String?,
-        userRole: UserRole = UserRole.CREW
     ): FlightsViewModel {
       return viewModel<FlightsViewModel>(
           factory =
               object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                  return FlightsViewModel(repository, userId, userRole) as T
+                  return FlightsViewModel(repository, userId) as T
                 }
               })
     }
@@ -51,6 +53,7 @@ class FlightsViewModel(
   private val _currentFlightTypes: MutableStateFlow<List<FlightType>> =
       MutableStateFlow(emptyList())
   private val _currentVehicles: MutableStateFlow<List<Vehicle>> = MutableStateFlow(emptyList())
+    private val _currentUser = MutableStateFlow<User?>(null)
 
   val currentFlights = _currentFlights.asStateFlow()
   val currentBalloons = _currentBalloons.asStateFlow()
@@ -59,12 +62,27 @@ class FlightsViewModel(
   val currentVehicles = _currentVehicles.asStateFlow()
 
   fun refresh() {
-    refreshCurrentFlights()
+      refreshUserAndFlights()
     refreshCurrentBalloons()
     refreshCurrentBaskets()
     refreshCurrentFlightTypes()
     refreshCurrentVehicles()
   }
+
+    fun refreshUserAndFlights() {
+        viewModelScope.launch {
+            _currentUser.value = repository.userTable.get(userId?: UNSET_ID, onError = { onError(it) })
+            if (_currentUser.value is Admin) {
+                _currentFlights.value = repository.flightTable.getAll(onError = { onError(it) })
+            } else if (_currentUser.value is Pilot || _currentUser.value is Crew){
+                _currentFlights.value = repository.userTable.retrieveAssignedFlights(
+                    repository.flightTable,
+                    userId?: UNSET_ID,
+                    onError = { onError(it)}
+                )
+            }
+        }
+    }
 
   fun refreshCurrentBalloons() =
       viewModelScope.launch {
@@ -84,19 +102,6 @@ class FlightsViewModel(
   fun refreshCurrentFlightTypes() =
       viewModelScope.launch {
         _currentFlightTypes.value = repository.flightTypeTable.getAll(onError = { onError(it) })
-      }
-
-  fun refreshCurrentFlights() =
-      viewModelScope.launch {
-          if (userRole == UserRole.ADMIN) {
-              _currentFlights.value = repository.flightTable.getAll(onError = { onError(it) })
-          } else {
-              _currentFlights.value = repository.userTable.retrieveAssignedFlights(
-                  repository.flightTable,
-                  userId?: UNSET_ID,
-                  onError = { onError(it)}
-              )
-          }
       }
 
   /**
