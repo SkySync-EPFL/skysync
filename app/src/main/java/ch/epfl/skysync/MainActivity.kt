21db01130aa417e03c3e28a6c75e0be4afef9f05
Alexandre Goumaz <alexandre.k.goumaz@gmail.com>
2024-05-09 10:58:52 +0200

    create UserGlobalViewModel

--- app/src/main/java/ch/epfl/skysync/MainActivity.kt
+++ app/src/main/java/ch/epfl/skysync/MainActivity.kt
@@ -7,65 +7,38 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.mutableStateOf
 import androidx.navigation.compose.rememberNavController
 import ch.epfl.skysync.components.GlobalSnackbarHost
-import ch.epfl.skysync.components.SnackbarManager
 import ch.epfl.skysync.database.FirestoreDatabase
 import ch.epfl.skysync.navigation.MainGraph
 import ch.epfl.skysync.ui.theme.SkySyncTheme
 import ch.epfl.skysync.viewmodel.TimerViewModel
+import ch.epfl.skysync.viewmodel.UserGlobalViewModel
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
   private lateinit var signInLauncher: ActivityResultLauncher<Intent>
-  private val userId = mutableStateOf<String?>(null)
+  private var userGlobalViewModel: UserGlobalViewModel? = null
   private val db: FirestoreDatabase = FirestoreDatabase()
   private val repository: Repository = Repository(db)
 
-  private fun onError(e: Exception) {
-    SnackbarManager.showMessage(e.message ?: "An unknown error occurred")
-  }
-
   private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+    val vm = userGlobalViewModel ?: return
     if (result.resultCode == RESULT_OK) {
-      val incomingUser = FirebaseAuth.getInstance().currentUser!!
-      val email = incomingUser.email!!
-
-      runBlocking {
-        val userExists = repository.userTable.get(incomingUser.uid)
-        if (userExists != null) {
-          userId.value = incomingUser.uid
-
-          return@runBlocking
-        }
-
-        val tempUser = repository.tempUserTable.get(email)
-        if (tempUser != null) {
-          repository.userTable.set(
-              incomingUser.uid, tempUser.toUserSchema(incomingUser.uid).toModel())
-          repository.tempUserTable.delete(email)
-          userId.value = incomingUser.uid
-        } else {
-          userId.value = "default-user"
-        }
-      }
+      val user = FirebaseAuth.getInstance().currentUser!!
+      vm.loadUser(user.uid, user.email!!)
     }
-
-    val snackBarText =
-        if (userId.value == "default-user") "Authentication with default Admin user"
-        else "Authentication Successful"
-    SnackbarManager.showMessage(snackBarText)
   }
 
   @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    // Initialize the signInLauncher
+    // Initialize the signInLauncher here
+    // as it needs to be created in MainActivity before the application is started,
+    // that is before the setContent method is called
     signInLauncher =
         registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
           this.onSignInResult(res)
@@ -75,12 +48,13 @@ class MainActivity : ComponentActivity() {
       SkySyncTheme {
         val navController = rememberNavController()
         Scaffold(snackbarHost = { GlobalSnackbarHost() }) {
+          userGlobalViewModel = UserGlobalViewModel.createViewModel(repository)
           val timerVm = TimerViewModel.createViewModel() // is shared between all screens
           MainGraph(
               repository = repository,
               navHostController = navController,
               signInLauncher = signInLauncher,
-              uid = userId.value,
+              userGlobalViewModel = userGlobalViewModel!!,
               timer = timerVm,
           )
         }
