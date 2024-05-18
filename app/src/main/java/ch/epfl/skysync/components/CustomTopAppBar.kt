0254a1f922d58696a6c7c29d4ed3c92681440df7
Prolas <timo.moebel@hotmail.com>
2024-05-18 13:41:28 +0200

    fix spam back click button

--- app/src/main/java/ch/epfl/skysync/components/CustomTopAppBar.kt
+++ app/src/main/java/ch/epfl/skysync/components/CustomTopAppBar.kt
@@ -9,18 +9,27 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTopAppBar(navController: NavController, title: String) {
+  val navBackStackEntry by navController.currentBackStackEntryAsState()
+  val currentDestination = navBackStackEntry?.destination
   TopAppBar(
       title = { Text(text = title, style = MaterialTheme.typography.headlineMedium) },
       navigationIcon = {
         IconButton(
-            onClick = { navController.popBackStack() }, modifier = Modifier.testTag("BackButton")) {
+            onClick = {
+              if (!navController.popBackStack()) {
+                currentDestination?.route?.let { navController.navigate(it) }
+              }
+            },
+            modifier = Modifier.testTag("BackButton")) {
               Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
             }
       })
