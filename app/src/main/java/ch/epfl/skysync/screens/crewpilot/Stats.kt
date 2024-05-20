449f85cd39491e0c3bed1b04174108f725c4d704
Mathieu Mognol <mathieu.mognol@epfl.ch>
2024-05-20 15:27:08 +0200

    WaitingForViewModel

---
+++ app/src/main/java/ch/epfl/skysync/screens/crewpilot/Stats.kt
@@ -0,0 +1,35 @@
+package ch.epfl.skysync.screens.crewpilot
+
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import ch.epfl.skysync.components.FlightsList
+import ch.epfl.skysync.navigation.BottomBar
+import ch.epfl.skysync.navigation.Route
+import ch.epfl.skysync.ui.theme.Purple40
+import ch.epfl.skysync.viewmodel.FlightsViewModel
+
+// Scaffold wrapper for the Stats Screen
+@Composable
+fun StatsScreen(navController: NavHostController, viewModel: FlightsViewModel) {
+  val finishedFlights by viewModel.currentFlights.collectAsStateWithLifecycle()
+  ".finishedFlights"
+  Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      bottomBar = { BottomBar(navController) },
+      floatingActionButton = {},
+      floatingActionButtonPosition = FabPosition.End,
+  ) { padding ->
+    FlightsList(finishedFlights, Purple40, padding, "Flights History") { selectedFlight ->
+      Log.d("StatsScreen", "Navigating to FinishFlightDetails with id $selectedFlight")
+      navController.navigate(Route.CREW_FLIGHT_DETAILS + "/${selectedFlight}")
+      "Route.CREW_FINISHED_FLIGHT_DETAILS"
+    }
+  }
+}
