97d46d0bbd4a1df113a86aa06a97ab91c49fd262
rafi <rafael.sollberger@gmail.com>
2024-05-29 10:36:30 +0200

    added haversine distance

--- app/src/main/java/ch/epfl/skysync/models/location/FlightTrace.kt
+++ app/src/main/java/ch/epfl/skysync/models/location/FlightTrace.kt
@@ -1,8 +1,15 @@
 package ch.epfl.skysync.models.location
 
 import ch.epfl.skysync.models.UNSET_ID
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 data class FlightTrace(
     val id: String = UNSET_ID,
     val trace: List<LocationPoint>,
 )
+
+
+
--- app/src/main/java/ch/epfl/skysync/models/location/LocationPoint.kt
+++ app/src/main/java/ch/epfl/skysync/models/location/LocationPoint.kt
@@ -1,6 +1,11 @@
 package ch.epfl.skysync.models.location
 
 import com.google.android.gms.maps.model.LatLng
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 /** @param time The time elapsed since start of flight in seconds */
 data class LocationPoint(
@@ -9,9 +14,35 @@ data class LocationPoint(
     val longitude: Double,
     val name: String = ""
 ) {
-  fun latlng(): LatLng = LatLng(latitude, longitude)
+    fun latlng(): LatLng = LatLng(latitude, longitude)
+    fun distanceTo(other: LocationPoint): Double {
+        return haversine(latitude, longitude, other.latitude, other.longitude)
+    }
 
-  companion object {
-    val UNKNONWN_POINT = LocationPoint(-1, 0.0, 0.0)
-  }
+    companion object {
+        val UNKNONWN_POINT = LocationPoint(-1, 0.0, 0.0)
+
+    }
+}
+
+
+fun haversine(
+    lat1: Double, lon1: Double,
+    lat2: Double, lon2: Double
+): Double {
+    // distance between latitudes and longitudes
+
+    val dLat = Math.toRadians(lat2 - lat1)
+    val dLon = Math.toRadians(lon2 - lon1)
+
+
+    // convert to radians
+    val lat1Rad = Math.toRadians(lat1)
+    val lat2Rad = Math.toRadians(lat2)
+
+    // apply formulae
+    val a: Double = sin(dLat / 2).pow(2.0) + sin(dLon / 2).pow(2.0) * cos(lat1Rad) * cos(lat2Rad)
+    val rad = 6371.0
+    val c = 2 * asin(sqrt(a))
+    return rad * c
 }
